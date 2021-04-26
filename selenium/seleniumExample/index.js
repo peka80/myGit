ccb85895191cdf3d0b2d56663a22281708d537f6
peka80 <petar.dragovic@gmail.com>
2021-04-26 22:35:30 +0200

    dodavanje komentara

--- selenium/seleniumExample/index.js
+++ selenium/seleniumExample/index.js
@@ -1,7 +1,7 @@
 const { Builder, By, Key, util } = require('selenium-webdriver');
 async function example() {
-  let driver = await new Builder().forBrowser('chrome').build();
-  await driver.get('http://www.google.com');
-  await driver.findElement(By.name('q')).sendKeys("Guns N' Roses", Key.RETURN);
+  let driver = await new Builder().forBrowser('chrome').build(); // definisanje pretrazivaca
+  await driver.get('http://www.google.com'); // koju adresu da otvori kada pokrene pretrazivac
+  await driver.findElement(By.name('q')).sendKeys("Guns N' Roses", Key.RETURN); // ukucavanje zeljenog termina u search polje Google-a
 }
 example();
